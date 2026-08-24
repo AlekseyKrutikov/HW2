6f63b13596972af7d038e4fb830383e655eec7a4
Aleksey <Nouni00@mail.ru>
2026-08-24 19:06:27 +0500

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,31 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      // by name
+      if (action.payload === "up") {
+        const sorted = [...state].sort((a, b) =>
+          a.name.localeCompare(b.name, "ru"),
+        );
+        return sorted;
+      } else if (action.payload === "down") {
+        const sorted = [...state].sort((a, b) =>
+          b.name.localeCompare(a.name, "ru"),
+        );
+        return sorted;
+      }
     }
-}
+    case "check": {
+      const filterUsers = state.filter((item) => item.age > 18);
+      return filterUsers; // need to fix
+    }
+    default:
+      return state;
+  }
+};
